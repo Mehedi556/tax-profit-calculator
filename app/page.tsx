4bfca52e9a3a7081ae783ec937ec46f30aa2f07e
Abdus Salam Mehedi <salam.mehedi99@gmail.com>
2024-01-30 11:06:25 +0600

    first commit for create repository

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,114 @@
+'use client'
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const [productPrice, setProductPrice] = useState(0);
+  const [profitAmount, setProfitAmount] = useState(0);
+  const [profitPercentage, setProfitPercentage] = useState(0);
+  const [taxPercentage, setTaxPercentage] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const calculateProfitAmount = (percentage: number) => {
+    return (productPrice * percentage) / 100;
+  };
+
+  const calculateProfitPercentage = (amount: number) => {
+    return (amount / productPrice) * 100;
+  };
+
+  const calculateTotal = () => {
+    const totalProfit = productPrice + profitAmount;
+    const taxAmount = (taxPercentage / 100) * totalProfit;
+    const totalAmount = totalProfit + taxAmount;
+    setTotal(totalAmount);
+  };
+
+  useEffect(() => {
+    calculateTotal();
+  }, [productPrice, profitAmount, taxPercentage]);
+
+  const handleProfitAmountChange = (e: any) => {
+    const amount = parseFloat(e.target.value);
+    setProfitAmount(amount);
+    setProfitPercentage(calculateProfitPercentage(amount));
+  };
+
+  const handleProfitPercentageChange = (e: any) => {
+    const percentage = parseFloat(e.target.value);
+    setProfitPercentage(percentage);
+    setProfitAmount(calculateProfitAmount(percentage));
+  };
+
+  const handleTaxChange = (e: any) => {
+    const tax = parseFloat(e.target.value);
+    setTaxPercentage(tax);
+  };
+  
+  return (
+    <main className='flex justify-center items-center w-full h-screen'>
+      
+      <div className='w-11/12 sm:w-8/12 md:w-7/12 lg:w-4/12 xl:w-3/12 mx-auto border rounded-lg p-10'>
+        <h1 className='font-bold text-xl'>Tax Calculation</h1>
+        <div className='flex flex-col gap-y-1 mt-5'>
+          <label className='text-sm'>Product Price: </label>
+        <input 
+          type="number"
+          value={productPrice}
+          onChange={(e) => setProductPrice(parseFloat(e.target.value))}
+          className='text-sm focus:outline-second pl-4 pr-4 py-2 border-2 border-gray-200 rounded w-full'
+        />
+        </div>
+      
+      
+
+      <div className='flex flex-col md:flex-row gap-x-5'>
+        <div className='flex flex-col gap-y-1 mt-5'>
+          <label className='text-sm'>Profit Amount: </label>
+          <input 
+            type="number" 
+            value={profitAmount} 
+            onChange={handleProfitAmountChange}
+            className='text-sm focus:outline-second pl-4 pr-4 py-2 border-2 border-gray-200 rounded w-full'
+          />
+        </div>
+        
+        <div className='flex flex-col gap-y-1 mt-5'>
+          <label className='text-sm'>Profit Percentage: </label>
+          <input 
+            type="number" 
+            value={profitPercentage} 
+            onChange={handleProfitPercentageChange}
+            className='text-sm focus:outline-second pl-4 pr-4 py-2 border-2 border-gray-200 rounded w-full'
+          />
+        
+        </div>
+        
+      </div>
+      <div className='flex flex-col gap-y-1 mt-5'>
+        <label className='text-sm'>Tax Percentage: </label>
+        <input 
+          type="number" 
+          value={taxPercentage} 
+          onChange={handleTaxChange}
+          className='text-sm focus:outline-second pl-4 pr-4 py-2 border-2 border-gray-200 rounded w-full'
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      
+      
+      <div className='flex flex-col gap-y-1 mt-5'>
+        <label className='text-sm'>Total (with Tax if provided): </label>
+        <input 
+          type="number" 
+          value={total} 
+          readOnly
+          className='text-sm focus:outline-second pl-4 pr-4 py-2 border-2 border-gray-200 rounded w-full'
+        />
       </div>
+      
+      
+    </div>
     </main>
   )
 }
